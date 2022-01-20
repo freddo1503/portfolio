82edd411e270f47003f89d2a6768fafce8cce751
freddo1503 <fredericferrera@yahoo.fr>
2022-01-20 16:02:02 +0000

    setup devcontainer env

--- app/portfolio/settings.py
+++ app/portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ "default": {
+ "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+ "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+ "USER": os.environ.get("SQL_USER", "user"),
+ "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+ "HOST": os.environ.get("SQL_HOST", "localhost"),
+ "PORT": os.environ.get("SQL_PORT", "5432"),
+ }
 }
 
 
